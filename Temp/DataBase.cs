31478fd6864b4980b3396b99c0c24e0419c59d29
PotatX <github.com/PotatX>
2024-05-23 13:43:39 +0400

    ef

---
+++ Temp/DataBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WinFormsApp1
+{
+    public class DataBase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Initial Catalog=DataBase");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual DbSet<Pacient> Pacients { get; set; }
+        public virtual DbSet<Hospital> Hospitals { get; set; }
+    }
+}
